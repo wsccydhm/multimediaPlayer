e6b15fe1e10e1b4e5f4d0098fbb62fc651d1196c
CCY <wsccydhm@163.com>
2018-02-26 09:55:58 +0800

    创建项目

---
+++ multimediaPlayer/multimediaPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  multimediaPlayer
+//
+//  Created by ccy on 2018/2/26.
+//  Copyright © 2018年 ccy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ multimediaPlayer/multimediaPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  multimediaPlayer
+//
+//  Created by ccy on 2018/2/26.
+//  Copyright © 2018年 ccy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
